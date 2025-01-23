58b9907689db7b3b47d27b344abe83e9340b7d6d
engsr6982 <pwxg286704@163.com>
2025-01-23 14:47:24 +0800

    feat: remove unused foo function and commands

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,13 @@
-import foo from "./foo.js";
-
 JSE.registerPlugin({
-    name: "my_plugin",
-    version: "1.0.0",
-    load: Enums.PluginLoadOrder.PostWorld,
-    permissions: {
-        "myPlugin.command": {
-            description: "My plugin command",
-            default: Enums.PermissionDefault.True,
-        },
-    },
-    commands: {
-        hello: {
-            description: "Says hello",
-            usages: ["/hello"],
-            permissions: ["myPlugin.command"],
-        },
-    },
-
-    onLoad: () => {
-        console.log("Plugin loaded!");
-    },
-    onEnable: () => {
-        console.log("Plugin enabled!");
-        foo(); // Call the foo function
-    },
-    onDisable: () => {
-        console.log("Plugin disabled!");
-    },
-    onCommand: (sender) => {
-        sender.sendMessage("Hello!");
-        return true;
-    },
+  name: "my_plugin",
+  version: "1.0.0",
+  onLoad: () => {
+    console.log("Plugin loaded!");
+  },
+  onEnable: () => {
+    console.log("Plugin enabled!");
+  },
+  onDisable: () => {
+    console.log("Plugin disabled!");
+  },
 });
